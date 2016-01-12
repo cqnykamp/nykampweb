f561ff075d16304219f28f5a7bc6a917f1d53d3b
Charles Nykamp <cqnykamp@gmail.com>
2016-01-12 10:56:12 -0600

    Added ability to toggle 'completed' in task index view

--- tasklist/urls.py
+++ tasklist/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from .views import TaskIndexView
+from .views import TaskIndexView, ToggleCompletedView
 
 urlpatterns = [
     url(r'^$', TaskIndexView.as_view(), name="index"),
+    url(r'^toggle_completed$', ToggleCompletedView.as_view(), name="toggle_completed"),
 
 ]
--- tasklist/views.py
+++ tasklist/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, View
+from django.http import JsonResponse
+
 from .models import Item
 
 class TaskIndexView (ListView):
-	queryset = Item.objects.order_by('start_date').filter(completed=False)
+	queryset = Item.objects.order_by('start_date')
 	template_name='tasklist/index.html'
+
+
+class ToggleCompletedView (View):
 	
+	def post(self, request, *args, **kwargs):
+	
+		try:
+			item_id = request.POST['id']
+		except KeyError:
+			return JsonResponse({})
+	
+		try:
+			item = Item.objects.get(id = item_id)
+		except Item.DoesNotExist:
+			return JsonResponse({})
+		
+		item.completed = not item.completed
+		item.save()
+		
+		return JsonResponse({'item_id': item_id, 'completed': item.completed})
